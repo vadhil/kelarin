0db8764534294313b8e96243c5c6a1daf257f32b
fadhil <88018547+vadhil@users.noreply.github.com>
2023-05-07 12:43:22 +0800

    second

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -1,7 +1,10 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [nav, setNav] = useState(true)
   return (
     <header className="container p-4">
         <nav className="row flex items-center justify-between ">
@@ -10,10 +13,13 @@ export default function Navbar() {
               <Image className="p-4" src={"next.svg"} width={100} height={100} alt="logo-aex"/>
               </a>
             </div>
-          <div className="nav-items flex gap-4">
+          <div className={`sm:block nav-items sm:flex gap-4 ${nav && "hidden"}`}>
             <Link href={"/"}>beranda</Link>
             <Link href={"/testimoni"}>testimoni</Link>
             <Link href={"/tentang"}>Tentang Kami</Link>
+          </div>
+          <div className="nav-button">
+              <button onClick={()=>setNav(!nav)} className="btn block md:hidden bg-blue-950 text-white p-2 rounded-md">menu</button>
           </div>
             
         </nav>
